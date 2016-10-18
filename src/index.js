0ce466c811c08ccf53ad1dffdfbfbb8dd0afbff5
holzru <holzru@gmail.com>
2016-10-18 16:00:27 -0700

    debounced

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyD0XiN2OdvqCjtchY5o76lzFulbOWnScM8';
 
@@ -13,15 +15,19 @@ class App extends Component {
     super(props);
 
     this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('surfboards')
+  }
 
-    YTSearch({key: API_KEY, term:"surfboards"}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term:term}, (videos) => {
       this.setState({videos: videos, selectedVideo: videos[0]});
     });
   }
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           videos={this.state.videos}
