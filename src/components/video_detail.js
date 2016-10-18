9bc27734da094325eae90d1a4c9818a15c7cea88
holzru <holzru@gmail.com>
2016-10-18 15:15:00 -0700

    counting

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 
-const VideoList = () => {
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
   return (
-    <ul className="col-md-4 list-group">
-    </ul>
-  );
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className=" embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className='details'>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  )
 }
+
+export default VideoDetail;
