5e53e3693ec107365893ce8ca30ab1b2684487ec
holzru <holzru@gmail.com>
2016-10-18 15:29:20 -0700

    videos showing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyD0XiN2OdvqCjtchY5o76lzFulbOWnScM8';
 
@@ -11,17 +12,20 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [], selectedVideo: null };
 
     YTSearch({key: API_KEY, term:"surfboards"}, (videos) => {
-      this.setState({videos});
+      this.setState({videos: videos, selectedVideo: videos[0]});
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
       </div>
     );
   }
